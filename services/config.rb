5a0efc7e06ea30ac08a67228d1f93499cadd6719
nursultan <nursultan@cloudcoure.com>
2017-04-18 11:33:34 +0600

    updated

--- services/config.rb
+++ services/config.rb
@@ -10,8 +10,8 @@
 ## Create a vpc, igw and attach the igw to the vpc
 ######################################################################
 coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
-  action :sustain
   cidr "${VPC_OCTETS}/16"
   internet_gateway true
   region "${REGION}"
   tags ${VPC_TAGS}
+end
